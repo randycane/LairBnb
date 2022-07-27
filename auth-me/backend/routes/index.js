3e2cdb3a1c74cacd4de2f96250a0d5cde78e2182
Randy <randychang97@gmail.com>
2022-07-27 14:47:25 -0700

    token

--- auth-me/backend/routes/index.js
+++ auth-me/backend/routes/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+// router.get('/hello/world', function(req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
 
-// above is setting a cookie on the response with the name of XSRF-TOKEN to the value of the req.csrfToken method's return.
+// above is a test: setting a cookie on the response with the name of XSRF-TOKEN to the value of the req.csrfToken method's return.
+
+// Add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+      'XSRF-Token': csrfToken
+    });
+});
 
 module.exports = router;

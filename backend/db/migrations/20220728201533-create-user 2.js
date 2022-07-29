8cec0dd02ba58f78100c2a864b9b1abd6876c1e6
Randy <randychang97@gmail.com>
2022-07-29 15:30:06 -0700

    for deploy

---
+++ backend/db/migrations/20220728201533-create-user 2.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  up: async(queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      hashedPassword: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      }
+    });
+  },
+  down: async(queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};

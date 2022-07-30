72914aaee089e60176da802097c974a5c0609356
Randy <randychang97@gmail.com>
2022-07-30 16:44:16 -0700

    add associations

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -49,6 +49,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.hasMany(models.Spot, { foreignKey: 'ownerId' })
+      User.hasMany(models.Review, { foreignKey: 'userId' })
+      User.hasMany(models.Booking, {foreignKey: 'userId'})
+
     }
   }
   User.init({

feb93920fc60c15076baeb849b2b2f4f128bdd9b
Randy <randychang97@gmail.com>
2022-08-19 14:25:28 -0700

    log

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,6 +10,13 @@ import App from './App';
 import configureStore from './store';
 
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
+
+import sessionReducer from './session';
+
+const rootReducer = combineReducers({
+  session: sessionReducer,
+});
 
 const store = configureStore();
 
@@ -18,6 +25,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {

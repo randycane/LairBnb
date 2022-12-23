f443158aaf766d39476ed67700709b2bbda9338d
randycane <randychang97@gmail.com>
2022-12-22 19:12:38 -0800

    book action

--- frontend/src/store/bookings.js
+++ frontend/src/store/bookings.js
@@ -1,1 +1,9 @@
 import { csrfFetch } from './csrf';
+
+//Action creators
+const LOAD_BOOK = '/bookings/LOAD';
+const CREATE_BOOK = '/bookings/CREATE';
+const UPDATE_BOOK = '/bookings/UPDATE';
+const DELETE_BOOK = '/bookings/DELETE';
+
+const LOAD_MY_BOOKS = '/bookings/LOAD_BY_USER';

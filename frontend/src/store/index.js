b789a75d98cabac936e8e94535845c86dfacae82
randycane <randychang97@gmail.com>
2022-12-27 15:54:59 -0800

    add to root reducer

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,12 +3,14 @@ import thunk from 'redux-thunk';
 import sessionReducer from './session';
 import spotsReducer from './spots';
 import reviewsReducer from './reviews';
+import bookingsReducer from './bookings';
 
 
 const rootReducer = combineReducers({
   session: sessionReducer,
   spots: spotsReducer,
   reviews: reviewsReducer,
+  bookings: bookingsReducer
 });
 
 

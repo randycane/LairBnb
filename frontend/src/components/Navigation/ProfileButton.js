746e014e7fd68e7939cd06320aa5ea493cc29738
randycane <randychang97@gmail.com>
2023-01-09 16:26:53 -0800

    profile properties

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,16 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from 'react-redux';
-import { NavLink, useHistory } from "react-router-dom";
-import * as sessionActions from '../../store/session';
-
-
+import { useDispatch } from "react-redux";
+import { NavLink, useHistory, Link } from "react-router-dom";
+import * as sessionActions from "../../store/session";
 
 function ProfileButton({ user }) {
-
   const history = useHistory();
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
-
 
   const openMenu = () => {
     if (showMenu) return;
@@ -24,40 +20,58 @@ function ProfileButton({ user }) {
       setShowMenu(false);
     };
 
-    document.addEventListener('click', closeMenu)
+    document.addEventListener("click", closeMenu);
 
     return () => document.removeEventListener("click", closeMenu);
   }, [showMenu]);
 
   const logout = (e) => {
     e.preventDefault();
-    dispatch(sessionActions.logout())
-      .then(() => history.push('/'));
+    dispatch(sessionActions.logout()).then(() => history.push("/"));
   };
 
+  return (
+    <div className="NavBarRight-MenuHost">
+      {user && (
+        <NavLink className="HostButton" to="/spots/new">
+          Become a Host
+        </NavLink>
+      )}
+      <button className="DropDownMenuIcon" onClick={openMenu}>
+        <i className="fas fa-bars" /> <i className="fas fa-user-circle" />
+      </button>
 
-    return (
-      <div className="NavBarRight-MenuHost">
-        { user && <NavLink className='HostButton' to='/spots/new'>Become a Host</NavLink> }
-          <button className="DropDownMenuIcon" onClick={openMenu}>
-            <i className="fas fa-bars"/> <i className="fas fa-user-circle"/>
-            </button>
-
-        {showMenu && (
-          <div className="profile-dropdown">
-            <div className="top">Welcome, {user.username}!</div>
-            <div className="top">Profile: {user.email}</div>
+      {showMenu && (
+        <div className="profile-dropdown">
+          <div className="top">Welcome, {user.username}!</div>
+          <div className="top">Profile: {user.email}</div>
           <div className="NaviBarShowing">
-            { user && (
-              <div className="UserBarNavi" >
-              <NavLink  className="Logout" to='/' onClick={logout}>Log out</NavLink>
+            {user && (
+              <div className="UserBarNavi">
+                <div className="top"><Link to="/currentUser/spots" id="my-spots">
+                  My Properties
+                </Link>
+                </div>
+                <div className="top">
+                <Link to="/currentUser/bookings" id="my-bookings">
+                  My Bookings
+                  </Link>
+                </div>
+                <div className="top">
+                <Link to="/spots/currentUser/reviews" id="my-reviews">
+                  My Reviews
+                  </Link>
+                  </div>
+                <NavLink className="Logout" to="/" onClick={logout}>
+                  Log out
+                </NavLink>
               </div>
             )}
           </div>
-          </div>
-        )}
-      </div>
-    );
-  }
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default ProfileButton;

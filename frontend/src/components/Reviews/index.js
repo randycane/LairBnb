915d6c488421f3d98f4212ef59ebb29dfb4734f6
randycane <randychang97@gmail.com>
2023-02-05 22:56:39 -0800

    link my reviews

--- frontend/src/components/Reviews/index.js
+++ frontend/src/components/Reviews/index.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import React, { useEffect, useState } from "react";
-import { getMyOwnReviewsThunk} from "../../store/reviews";
+import { NavLink } from "react-router-dom";
+import { getMyOwnReviewsThunk } from "../../store/reviews";
 import { removeReviewsThunk } from "../../store/reviews";
 
 import ReviewsCard from "./ReviewsCards";
@@ -8,33 +9,32 @@ import ReviewsCard from "./ReviewsCards";
 // my own reviews:
 //use this to both read and delete:
 function UserReviewsComponent() {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const selectedReviews = useSelector((state) => state.reviews);
 
+  const normalizedReviews = Object.values(selectedReviews);
 
-    const selectedReviews = useSelector((state) => state.reviews);
+  useEffect(() => {
+    dispatch(getMyOwnReviewsThunk());
+    dispatch(removeReviewsThunk());
+  }, [dispatch]);
 
-    const normalizedReviews = Object.values(selectedReviews);
-
-
-    useEffect(() => {
-        dispatch(getMyOwnReviewsThunk());
-        dispatch(removeReviewsThunk());
-    }, [dispatch]);
-
-    return (
-        <>
-        <div className="reviews-container">
-            {normalizedReviews.map(review => (
-                <div className="review-details">
-                    <ReviewsCard review ={review}/>
-                    </div>
-            ))}
-
-            </div>
-            </>
-)
-
+  return (
+    <>
+      <div className="reviews-container">
+        <div className="reviewed-by-me">
+          {normalizedReviews?.length > 0 ? "My Reviews" : "No Reviews"}
+        </div>
+        {normalizedReviews.map((review) => (
+            <div className="review-details">
+                <NavLink className="spot-link" to={`/spots/${review.spotId}`}>Review:</NavLink>
+            <ReviewsCard review={review} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default UserReviewsComponent;

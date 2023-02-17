c38841094d89fcf723bf0c6de19a281ac064e838
randycane <randychang97@gmail.com>
2023-02-16 16:05:16 -0800

    remove unused states locations prints

--- frontend/src/components/Searched/SearchComponent.js
+++ frontend/src/components/Searched/SearchComponent.js
@@ -1,47 +1,49 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, Redirect, NavLink, useHistory} from "react-router-dom";
+import { Link, Redirect, NavLink, useHistory } from "react-router-dom";
 import { getSpotsThunk } from "../../store/spots";
 import SpotsCards from "../SpotsCards";
 import "./Searched.css";
 
 function SearchedComponent() {
-    const [spotsShowing, setSpotsShowing] = useState(false);
-    const [spotLoaded, setSpotLoaded] = useState(false);
-    const spots = useSelector((state) => state?.spots)
-    const normalizedSpots = Object.values(spots);
-    const dispatch = useDispatch();
-    const history = useHistory();
-    //const location = useLocation();
-    console.log("tjis should be an array", normalizedSpots)
-    useEffect(() => {
-        const url = new URL(window.location.href)
-        const searchParameters = url.searchParams;
-        (async () => {
-            let searchInput = searchParameters.get("input");
-            //console.log("searcghinput", searchInput)
-            await dispatch(getSpotsThunk(searchInput));
-            console.log("normalized updating", normalizedSpots)
-            setSpotsShowing(!spotsShowing);
-          })();
+  const [spotsShowing, setSpotsShowing] = useState(false);
+  const spots = useSelector((state) => state?.spots);
+  const normalizedSpots = Object.values(spots);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  //console.log("this should be an array", normalizedSpots)
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    const searchParameters = url.searchParams;
+    (async () => {
+      let searchInput = searchParameters.get("input");
+      //console.log("searcghinput", searchInput)
+      await dispatch(getSpotsThunk(searchInput));
+      //console.log("normalized updating", normalizedSpots)
+      setSpotsShowing(!spotsShowing);
+    })();
+  }, []);
 
-    },[])
-
-
-    return (
-        <>
-            {normalizedSpots.length ? <div className="nav-search">Search Results: </div> : <div className="in-search">No Results</div>}
-            <div className="property-of">
-                {normalizedSpots.map((spot) => {
-                    return <div className="spotsss">
-                        <NavLink to={`/spots/${spot.id}`}>
-                            <SpotsCards spot={spot}/>
-                        </NavLink>
-                    </div>
-                })}
-                </div>
-            </>
-    )
+  return (
+    <>
+      {normalizedSpots.length ? (
+        <div className="nav-search">Search Results: </div>
+      ) : (
+        <div className="in-search">No Results</div>
+      )}
+      <div className="property-of">
+        {normalizedSpots.map((spot) => {
+          return (
+            <div className="spotsss">
+              <NavLink to={`/spots/${spot.id}`}>
+                <SpotsCards spot={spot} />
+              </NavLink>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default SearchedComponent;

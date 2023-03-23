e676f07ac257ee3e00e7499bf288fe4222071ebc
randycane <randychang97@gmail.com>
2023-03-22 17:38:11 -0700

    update percentages

--- frontend/src/components/Bookings/DelBooking.js
+++ frontend/src/components/Bookings/DelBooking.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory, useParams, NavLink } from "react-router-dom";
-// import moment from "moment";
 
 import { getMyOwnBooksThunk, removeBookingThunk } from "../../store/bookings";
 import "./CreateBooking.css";
